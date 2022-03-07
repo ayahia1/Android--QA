52817f1bdb63e59b95752173fb1da8f458f237c1
Abdelrahman <ayahia1@swarthmore.edu>
2022-03-06 20:28:54 -0500

    Adding all bonus features

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
         TextView Question = findViewById(R.id.flashcard_question);
         TextView Answer = findViewById(R.id.flashcard_answer);
+        TextView Op1 = findViewById(R.id.option1);
+        TextView Op2 = findViewById(R.id.option2);
+        TextView Op3 = findViewById(R.id.option3);
+        ImageView On_eye = findViewById(R.id.on_eye);
+        ImageView Off_eye = findViewById(R.id.off_eye);
+
 
         Question.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +39,56 @@ public class MainActivity extends AppCompatActivity {
                 Question.setVisibility(View.VISIBLE);
             }
         });
-    }
 
+        Op1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Op1.setBackgroundColor(getResources().getColor(R.color.red));
+                Op2.setBackgroundColor(getResources().getColor(R.color.Chai));
+                Op3.setBackgroundColor(getResources().getColor(R.color.green));
+            }
+        });
+
+        Op2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Op1.setBackgroundColor(getResources().getColor(R.color.Chai));
+                Op2.setBackgroundColor(getResources().getColor(R.color.red));
+                Op3.setBackgroundColor(getResources().getColor(R.color.green));
+            }
+        });
+
+        Op3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Op1.setBackgroundColor(getResources().getColor(R.color.Chai));
+                Op2.setBackgroundColor(getResources().getColor(R.color.Chai));
+                Op3.setBackgroundColor(getResources().getColor(R.color.green));
+            }
+        });
+
+        On_eye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                On_eye.setVisibility(View.INVISIBLE);
+                Off_eye.setVisibility(View.VISIBLE);
+
+                Op1.setVisibility(View.VISIBLE);
+                Op2.setVisibility(View.VISIBLE);
+                Op3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        Off_eye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Off_eye.setVisibility(View.INVISIBLE);
+                On_eye.setVisibility(View.VISIBLE);
+
+                Op1.setVisibility(View.INVISIBLE);
+                Op2.setVisibility(View.INVISIBLE);
+                Op3.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
 }
